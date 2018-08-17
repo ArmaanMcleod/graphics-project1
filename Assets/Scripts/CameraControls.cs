9306cae1e1d3a21c62262e55753dca20547e66f7
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-08-17 13:58:01 +1000

    Completed camera script (Thank you Alice)

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -17,6 +17,8 @@ public class CameraControls : MonoBehaviour {
 
     public float sensitivity = 1;
 
+    public float moveSpeed = 1;
+
     // Use this for initialization
     void Start() {
         // This locks the cursor to the centre of the screen.
@@ -25,34 +27,49 @@ public class CameraControls : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        var cam = Camera.main.transform;
+        Rotate();
+        Move();
+    }
+
+    // Accept user rotation input
+    private void Rotate() {
         // control yaw
         currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
         currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
         // control pitch
         currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
         currentRotation.y = Mathf.Clamp(currentRotation.y, -80f, 80f);
-        Camera.main.transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);
-
+        transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);
     }
 
-    // Accept user input
+    // Accept user movement input
     private void Move() {
         // Move the camera
         if (Input.GetKey(KeyCode.W)) {
-
+            transform.position += relativeForward();
         }
 
         if (Input.GetKey(KeyCode.S)) {
-
+            transform.position -= relativeForward();
         }
 
         if (Input.GetKey(KeyCode.A)) {
-
+            transform.position -= relativeRight();
         }
 
         if (Input.GetKey(KeyCode.D)) {
-
+            transform.position += relativeRight();
         }
+
+    }
+
+    // Returns the relative forward vector scaled by the movement speed and time delta
+    private Vector3 relativeForward() {
+        return transform.forward * moveSpeed * Time.deltaTime;
+    }
+
+    // Returns the relative right vector scaled by the movement speed and time delta
+    private Vector3 relativeRight() {
+        return transform.right * moveSpeed * Time.deltaTime;
     }
 }
