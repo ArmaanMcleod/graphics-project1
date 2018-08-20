e68ab26e74a84c65f4e43b976dfec2b5e450cb8c
AL <alice.lin96@gmail.com>
2018-08-20 16:53:14 +1000

    camera collision w sphere and cube works

---
+++ Assets/Scripts/CameraCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Update () {
+		// Stops the rigid body from rebounding during a collision
+		this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+		
+	}
+
+	void OnCollisionEnter(Collision c){
+		Debug.Log(this.gameObject.GetComponent<Rigidbody>().velocity);
+		this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+	
+	}
+}
