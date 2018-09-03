be7ec36af729da37fdb959eb71994679f3aada3b
OpticGenius <opticgenius@hotmail.com>
2018-09-04 07:23:22 +1000

    Starting Sun rotation

--- Assets/Scripts/SunRotation.cs
+++ Assets/Scripts/SunRotation.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class SunRotation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        // Get size of terrain
+        GameObject terrainObject = GameObject.Find ("Terrain");
+        DiamondSquareTerrain terrain = terrainObject.GetComponent<DiamondSquareTerrain> ();
+        float terrainSize = terrain.GetSize ();
+
+    }
 }
